360254ab3da6c9aebce5f1206574226b6f3d9723
unknown <24882134+kkadhith@users.noreply.github.com>
2022-09-19 23:20:44 -0700

    init commit

---
+++ android/app/src/main/kotlin/com/example/photoui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.photoui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _selectedIndex = 0;
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  final List<Widget> _children = [
+  Center(child: Text("HOME")),
+    Center(child: Text("explore")),
+    Center(child: Text("SEARCH")),
+    Center(child: Text("REELS")),
+    Center(child: Text("PROFILE")),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        // onTap "returns" the index of each item, and _navigateBottomBar will change currIndex.
+        onTap: _navigateBottomBar,
+        // To increase the amount of items (>3)
+        type: BottomNavigationBarType.fixed,
+        items: [BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+                BottomNavigationBarItem(icon: Icon(Icons.explore), label: "explore"),
+                BottomNavigationBarItem(icon: Icon(Icons.search), label: "search"),
+                BottomNavigationBarItem(icon: Icon(Icons.video_call), label: "reels"),
+                BottomNavigationBarItem(icon: Icon(Icons.person), label: "profile")
+              ],
+      ),
+    );
+  }
+}
